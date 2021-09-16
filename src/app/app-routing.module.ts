da7be75fede566a8c33773b12ec6ef9a48f3b7fb
listen-whisper <listen-whisper@outlook.com>
2021-09-16 10:37:34 +0800

    feat(*): 模板更新顶部

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,6 @@
 import {NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {RouterComponent} from './views/router/router.component';
-import {ReadonlyMarkdownComponent} from './components/readonly-markdown/readonly-markdown.component';
-
-
-const routes: Routes = [
-  {
-    path: 'document',
-    component: RouterComponent,
-    children: [
-      {
-        path: 'quickstart', component: ReadonlyMarkdownComponent
-      }
-    ]
-  },
-  {
-    path: 'component',
-    component: RouterComponent,
-    children: [
-      {
-        path: 'announcement', component: ReadonlyMarkdownComponent,
-      }
-    ]
-  },
-  {
-    path: '',
-    redirectTo: 'document/quickstart',
-    pathMatch: 'full',
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/services/router-config.service.ts
+++ src/app/services/router-config.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {HeaderMenu, HeaderMenuList} from '../domains/header-menu.domain';
 import {Route, Router, Routes} from '@angular/router';
 import {MarkdownService} from './markdown.service';
+import {RouterComponent} from '../views/router/router.component';
+import {SideMenu} from '../domains/side-menu.domain';
+import {ReadonlyMarkdownComponent} from '../components/readonly-markdown/readonly-markdown.component';
 
 @Injectable({
   providedIn: 'root'
@@ -20,17 +23,53 @@ export class RouterConfigService {
 
   set headerMenuList(headerMenuList: HeaderMenuList){
     this._headerMenuList = headerMenuList;
+    this.routes = [];
+
+    let promise: any[] = [];
     headerMenuList.items.forEach(item => {
-      this.getMarkDown(item);
+      promise.push(this.getMarkDown(item));
     });
+
+    Promise.all(promise).then(() => {
+      const first = headerMenuList.first;
+      this.routes.push({
+        path: '',
+        redirectTo: first.Router.substring(1),
+        pathMatch: 'full',
+      })
+      this.router.resetConfig(this.routes);
+      this.router.navigate(['']).then();
+    })
   }
 
-  getMarkDown(headerMenu: HeaderMenu): void{
-    debugger
-    this.markdownService.getJsonConfig(headerMenu.Mark).then(res => {
-      const Route: Route = {
-        path: ''
-      };
+  getMarkDown(headerMenu: HeaderMenu){
+    return new Promise((resolve, reject) => {
+      this.markdownService.getJsonConfig<any>(headerMenu.Mark).then(res => {
+        let children: Routes = [];
+        let result: SideMenu[] = [];
+        try {
+          result = JSON.parse(res);
+        }catch (e){
+          result = res;
+        }
+        result.forEach(item => {
+          item.Children.forEach(router => {
+            children.push({
+              path: router.Mark,
+              component: ReadonlyMarkdownComponent
+            })
+          })
+        });
+        const route: Route = {
+          path: headerMenu.Mark,
+          component: RouterComponent,
+          children: children
+        };
+        this.routes.push(route);
+        resolve(true);
+      }).catch(err => {
+        reject(false);
+      })
     })
   }
 }
